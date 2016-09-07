71256ac26a114b29107f2ad09a5a8695694f81a8
John Steidley <johnsteidley@gmail.com>
2016-09-07 15:51:52 -0700

    add staticfile

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 extern crate iron;
 extern crate router;
+extern crate mount;
+extern crate staticfile;
 
 use iron::{Iron, Request, Response, IronResult};
 use iron::status;
 use router::{Router};
+use mount::Mount;
+use staticfile::Static;
+
+use std::path::Path;
 
 fn main() {
     let mut router = Router::new();
     router.get("/", handler, "handler");
     router.get("/:query", query_handler, "query_handler");
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    let mut mount = Mount::new();
+    mount
+        .mount("/api/v1/", router)
+        .mount("/", Static::new(Path::new("assets/")));
+
+    Iron::new(mount).http("127.0.0.1:3000").unwrap();
 
     fn handler(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "OK")))
+        Ok(Response::with((status::Ok, "OK, Connor")))
     }
 
     fn query_handler(req: &mut Request) -> IronResult<Response> {
@@ -21,6 +32,4 @@ fn main() {
             .unwrap().find("query").unwrap_or("/");
         Ok(Response::with((status::Ok, *query)))
     }
-
-
 }
